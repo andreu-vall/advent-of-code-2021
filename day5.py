aedddb69ab490431d595815d23141d608373a3ac
Andreu Vall <andreu.vallhernandez@gmail.com>
2021-12-05 11:40:53 +0100

    Update Leaderboard day4

--- day5.py
+++ day5.py
@@ -1,17 +1,48 @@
+from collections import Counter
 from pyperclip import copy
 import utils
 
 
 def get_lines():
-    return utils.get_input(4)
+    lines = utils.get_input(5)
+    return [list(map(int, line.replace(' -> ', ',').split(','))) for line in lines]
 
 
 def part1(lines):
-    return 0
+    c = Counter()
+    for x1, y1, x2, y2 in lines:
+        if x1 == x2:
+            for y in range(min(y1, y2), max(y1, y2)+1):
+                c[(x1, y)] += 1
+        elif y1 == y2:
+            for x in range(min(x1, x2), max(x1, x2)+1):
+                c[(x, y1)] += 1
+
+    return sum(val > 1 for val in c.values())
 
 
 def part2(lines):
-    return 0
+    c = Counter()
+    for x1, y1, x2, y2 in lines:
+        if x1 == x2:
+            for y in range(min(y1, y2), max(y1, y2) + 1):
+                c[(x1, y)] += 1
+        elif y1 == y2:
+            for x in range(min(x1, x2), max(x1, x2) + 1):
+                c[(x, y1)] += 1
+        else:
+            if x1 > x2:
+                x1, x2 = x2, x1
+                y1, y2 = y2, y1
+
+            direction = 1 if y2 > y1 else -1
+            x, y = x1, y1
+            while x <= x2:
+                c[(x, y)] += 1
+                x, y = x+1, y+direction
+            assert y-direction == y2
+
+    return sum(val > 1 for val in c.values())
 
 
 if __name__ == '__main__':
